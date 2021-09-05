5bd7d083b8c7bfc5fc7d0a8caefd109d921db110
fersuekinci <88900722+fersuekinci@users.noreply.github.com>
2021-09-05 04:25:36 +0300

    Create main.java

---
+++ ÖDEV14/main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee e1 = new Employee("a", 3000, 50, 2000);
+        Employee e2 = new Employee("b", 900, 20, 2015);
+
+        e1.toStringEmp();
+        e2.toStringEmp();
+    }
+}
